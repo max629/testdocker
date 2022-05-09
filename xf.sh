6bc6360ffb1943f07f24a7775f8c5bb41c5cddb6
max629 <max629@126.com>
2022-05-09 14:11:40 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "c905b321-c783-45bc-8f1c-55b4a4abdbf4",
                         "alterId": 0
                     }
                 ],
